9593df6b554b317ae4f1de9677f76e0064f38e34
Doreen Mensah <doreenradiance@gmail.com>
2021-01-30 17:02:04 +0000

    Imported the AppContainer component

--- App.js
+++ App.js
@@ -1,35 +1,12 @@
-import React from 'react';
-import ContactsScreen from "./src/screens/ContactsScreen"
-import LoginScreen from "./src/screens/LoginScreen"
-import SignupScreen from "./src/screens/SignupScreen"
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-
-const Stack = createStackNavigator()
+import React from "react";
+import {Provider} from "react-redux";
+import store from "./src/redux/store";
+import AppContainer from "./src/navigation/navigation";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{
-            header: () => null
-          }}
-          name='LoginScreen' component={LoginScreen} />
-
-        <Stack.Screen
-          options={{
-            header: () => null
-          }}
-          name='SignupScreen' component={SignupScreen} />
-
-        <Stack.Screen
-          // options={{
-          //   header: () => null
-          // }}
-          name='ContactsScreen' component={ContactsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}> 
+    <AppContainer/>
+    </Provider>
   )
 }
