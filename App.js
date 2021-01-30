b7d3adf0af21351af59a836544b310305f3995fb
Doreen Mensah <doreenradiance@gmail.com>
2021-01-30 15:35:57 +0000

    disabled the header for the contactsScreen

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ContactsScreen from "./src/screens/ContactsScreen"
+import LoginScreen from "./src/screens/LoginScreen"
+import SignupScreen from "./src/screens/SignupScreen"
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{
+            header: () => null
+          }}
+          name='LoginScreen' component={LoginScreen} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Stack.Screen
+          options={{
+            header: () => null
+          }}
+          name='SignupScreen' component={SignupScreen} />
+
+        <Stack.Screen
+          // options={{
+          //   header: () => null
+          // }}
+          name='ContactsScreen' component={ContactsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
