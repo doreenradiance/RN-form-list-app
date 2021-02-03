7a9a25a10f57957c5fcafe49eca282ae4519e2a4
Doreen Mensah <doreenradiance@gmail.com>
2021-02-03 15:20:03 +0000

    Created a case for the logout

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,14 +1,19 @@
 const initialState = {
-    login:false,
-    user:null
+    login: false,
+    user: null
 }
 
-export default (state,action)=>{
-switch(action.type){
-    case "LOGGED_IN":
-        return {
-            login:true, user:action.payload}
-    default:
-        return state;
-}
+export default (state, action) => {
+    switch (action.type) {
+        case "LOGGED_IN":
+            return {
+                login: true, user: action.payload
+            }
+        case "LOGGED_OUT":
+            return {
+                login: false, user: null
+            }
+        default:
+            return state;
+    }
 }
