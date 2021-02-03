50661c12b058fe7cf48b3043b15ce708cefd5366
Doreen Mensah <doreenradiance@gmail.com>
2021-02-03 14:54:58 +0000

    Created a variable for the login and the user, and created case to return them.

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,7 +1,13 @@
-
+const initialState = {
+    login:false,
+    user:null
+}
 
 export default (state,action)=>{
 switch(action.type){
+    case "LOGGED_IN":
+        return {
+            login:true, user:action.payload}
     default:
         return state;
 }
