eaa690820df898e46c1be2fa4bad4ad0f5209f9a
Doreen Mensah <doreenradiance@gmail.com>
2021-02-04 18:02:38 +0000

    Changed the typo of the persistedReducer to persistReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,14 @@ import {createStore,applyMiddleware} from "redux"
 import authReducer from "./reducers/authReducer"
 import {AsyncStorage} from "react-native";
 import  thunk  from "redux-thunk";
-import { persistStore,persistReducer } from "redux-persist";
+import {persistStore,persistReducer} from "redux-persist";
 
 const persistConfig = {
     key:"root",
     storage:AsyncStorage
 }
 
-const persistedReducer = persistedReducer (persistConfig,authReducer)
+const persistedReducer = persistReducer (persistConfig,authReducer)
 
 let store = createStore(persistedReducer,applyMiddleware(thunk))
 
